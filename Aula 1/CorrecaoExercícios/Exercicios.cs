99d89e479c78b8adcefea960ae2cceccb3328add
Eduardo Luan Pilonetto <eduardo.pilonetto@logae.com.br>
2026-07-20 21:58:43 -0300

    Correção Execícios

---
+++ Aula 1/CorrecaoExercícios/Exercicios.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrecaoExercicios
+{
+    //EXERCICIO 1
+    class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos.");
+        }
+    }
+
+    //EXERCICIO 2
+    class Carro
+    {
+        public string Modelo;
+        public int Velocidade = 0;
+        public void Acelerar()
+        {
+            Velocidade += 10;
+        }
+
+        public void Frear()
+        {
+            Velocidade -= 10;
+        }
+
+        public void VerVelocidade()
+        {
+            Console.WriteLine($"Velocidade Atual: {Velocidade}");
+        }
+    }
+    //ECERCÍCIO 3
+    class Produto
+    {
+        public string Nome;
+        public int Quantidade;
+        public string Preco;
+        
+        public void AdicionarQtd(int qtd) 
+        {
+            Quantidade = Quantidade + qtd;
+        }
+        public void RemoverQtd(int qtd)
+        {
+            Quantidade = Quantidade - qtd;
+        }
+        public int RetornaQtd()
+        {
+            return Quantidade;
+        }
+    }
+
+    //EXERCICIO 4
+    class Aluno
+    {
+        public string Nome;
+        public double Nota;
+        public void VerificarSituacao()
+        {
+            if (Nota < 5) Console.WriteLine("Reprovado");
+            else if (Nota < 7) Console.WriteLine("Em Recuperação");
+            else Console.WriteLine("Aprovado");
+        }
+    }
+
+    //EXERCÍCIO 5
+    class ContaBancaria
+    {
+        public string Titular;
+        public double Saldo = 0;
+        public void Depositar(double Valor)
+        {
+            Saldo += Valor;
+        }
+        public void Sacar(double Valor)
+        {
+            if(Saldo <= 0 || Saldo - Valor < 0)
+            {
+                Console.WriteLine("Saque indisponível");
+                return;
+            }
+            Saldo -= Valor;
+        }
+        public void MostrarSaldo()
+        {
+            Console.WriteLine($"Saldo Atual : {Saldo}");
+        }
+    }
+    //EXERCICIO 6
+    class Animal
+    {
+        public string Nome;
+        public string Som;
+        public string Especie;
+        public void EmitirSom()
+        {
+            Console.WriteLine(Som);
+        }
+    }
+    //EXERCICIO 7
+    class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+        public void AumentarSalario(double percentual)
+        {
+            Salario = Salario * (1 + (percentual / 100));
+            Console.WriteLine($"Novo Salário {Salario}");
+        }
+    }
+
+    //EXERCÍCIO 9
+    class Retangulo
+    {
+        public double Largura;
+        public double Autura;
+        public void CalcularArea()
+        {
+            Console.WriteLine($"A área do Retangulo é {Largura * Autura}");
+        }
+        public void CalcularPerimetro()
+        {
+            Console.WriteLine($"O perimetro do Retangulo é {(Largura * 2) + (Autura * 2)}");
+        }
+    }
+
+    //ECERCÍCIO 10
+    class Livro
+    {
+        public string Autor;
+        public string Titulo;
+        public bool Disponivel;
+        public void LivroEmprestado()
+        {
+            if (Disponivel)
+            {
+                Console.WriteLine("Livro não Emprestado");
+            }
+            else
+            {
+                Console.WriteLine("Livro Emprestado");
+            }
+        }
+
+    }
+}
---
+++ Aula 1/CorrecaoExercícios/Program.cs
@@ -0,0 +1,74 @@
+﻿using CorrecaoExercicios;
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("INSTANCA EXERCICIO 1");
+        Pessoa pessoa = new Pessoa();
+        pessoa.Nome = "Eduardo";
+        pessoa.Idade = 25;
+        pessoa.Apresentar();
+
+        Console.WriteLine("INSTANCA EXERCICIO 2");
+        Carro carro = new Carro();
+        carro.Modelo = "Opela";
+        carro.Acelerar();
+        carro.Acelerar();
+        carro.Frear();
+        carro.VerVelocidade();
+
+        Console.WriteLine("INSTANCA EXERCICIO 3");
+        Produto prod = new Produto();
+        prod.Nome = "Sabonete";
+        prod.AdicionarQtd(114);
+        prod.RemoverQtd(5);
+        prod.RemoverQtd(5);
+        Console.WriteLine($"Quantidade Atual: {prod.RetornaQtd()}");
+
+        Console.WriteLine("INSTANCA EXERCICIO 4");
+        Aluno alu = new Aluno();
+        alu.Nome = "Frederico";
+        alu.Nota = 6;
+        alu.VerificarSituacao();
+
+        Console.WriteLine("INSTANCA EXERCICIO 5");
+        ContaBancaria conta = new ContaBancaria();
+        conta.Titular = "Eduardo";
+        conta.Sacar(10);
+        conta.Depositar(20);
+        conta.Sacar(10);
+        conta.MostrarSaldo();
+
+        Console.WriteLine("INSTANCA EXERCICIO 6");
+        Animal animal = new Animal();
+        animal.Nome = "Javali";
+        animal.Som = "UiUiUi";
+        animal.Especie = "Argentino";
+        animal.EmitirSom();
+
+        Console.WriteLine("INSTANCA EXERCICIO 7");
+        Funcionario func = new Funcionario();
+        func.Nome = "Marco";
+        func.Salario = 1000;
+        func.AumentarSalario(14);
+
+        //Qual ficou mais organizado? O feito com POO
+        //Qual ficou mais fácil de entender? POO foi o mais fácil
+
+        Console.WriteLine("INSTANCA EXERCICIO 9");
+        Retangulo ret = new Retangulo();
+        ret.Largura = 3;
+        ret.Autura = 2;
+        ret.CalcularPerimetro();
+        ret.CalcularArea();
+
+        Console.WriteLine("INSTANCA EXERCICIO 10");
+        Livro liv = new Livro();
+        liv.Titulo = "A volta dos que não foram";
+        liv.Autor = "O autor";
+        liv.Disponivel = true;
+        liv.LivroEmprestado();
+    }
+}
