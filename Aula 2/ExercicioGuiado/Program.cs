bde303ab8e7cbe7247d87c011cead266ba68cdc2
Eduardo Luan Pilonetto <eduardo.pilonetto@logae.com.br>
2026-07-27 19:43:41 -0300

    Exercício Guiado

---
+++ Aula 2/ExercicioGuiado/Program.cs
@@ -0,0 +1,19 @@
+﻿Produto P1 = new Produto();
+P1.Nome = "Sabonete";
+P1.Preco = 2.99;
+P1.Quantidade = 10;
+P1.Ncm = "00012345";
+P1.Apresentar();
+
+class Produto
+{
+    public string Nome;
+    public double Preco;
+    public int Quantidade;
+    public string Ncm;
+
+    public void Apresentar()
+    {
+        Console.WriteLine($"Nome: {Nome}; Preco:{Preco}; Quantidade: {Quantidade}; NCM:{Ncm}");
+    }
+}
