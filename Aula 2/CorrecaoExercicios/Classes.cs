714a437ba4b551bc7e5ca96c4f5a6b8023d18f9e
Eduardo Luan Pilonetto <eduardo.pilonetto@logae.com.br>
2026-07-27 22:10:44 -0300

    Correção Exercícios

---
+++ Aula 2/CorrecaoExercicios/Classes.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Pessoa
+    {
+        public string Nome;
+        public string Cidade;
+        public int Idade;
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}\nCidade: {Cidade}\nIdade: {Idade}");
+        }
+    }
+
+    class Produto
+    {
+        public string Nome;
+        public int Quantidade;
+        public double Preco;
+
+        public void MostrarProduto()
+        {
+            Console.WriteLine($"Nome: {Nome}\n Quantidade {Quantidade}\n preço {Preco}");
+
+        }
+
+
+    }
+
+    class Carro
+    {
+        public string Modelo;
+        public string Marca;
+        public int Ano;
+        public string Cor;
+
+        public void MostrarCarro()
+        {
+            Console.WriteLine($"Meu carro {Cor} é um {Modelo} da marca {Marca} ano {Ano}");
+        }
+    }
+
+    class Aluno
+    {
+        public string? Nome;
+        public string? Curso;
+        public double Nota01;
+        public double Nota02;
+
+        public void CalcularMedia()
+        {
+            double notaFinal = (Nota01 + Nota02) / 2;
+            Console.WriteLine($"O aluno {Nome} do curso {Curso} ficou com a média final {notaFinal}");
+        }
+    }
+    class ContaBancaria
+    {
+        public string titular;
+        public double saldo;
+
+        public void Deposito(double valor)
+        {
+            saldo = saldo + valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            saldo = saldo - valor;
+        }
+
+        public void MostrarSaldo()
+        {
+            Console.WriteLine($"O valor do saldo é de: {saldo}");
+        }
+
+    }
+
+    class Funcionario
+    {
+        public string Nome;
+        public string Cargo;
+        public double Salario;
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}; Cargo: {Cargo}; Salario: {Salario}");
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            Salario = ((porcentagem / 100) + 1) * Salario;
+            Console.WriteLine($"Salario aumentado em {porcentagem}%");
+        }
+    }
+}
+
+public class Telefone
+
+{
+
+    public string Marca;
+    public string Modelo;
+    public int Bateria;
+    public bool Ligado;
+
+    public void MistrarBateria()
+    {
+        Console.WriteLine($"Bateria: {Bateria}%");    
+    }
+
+    public void Ligar()
+    {
+        Ligado = true;
+    }
+
+    public void Desligar()
+    {
+        Ligado = false;
+    }
+}
---
+++ Aula 2/CorrecaoExercicios/Program.cs
@@ -0,0 +1,61 @@
+﻿using Classes;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Pessoa pessoa = new Pessoa();
+        pessoa.Nome = "Carlos";
+        pessoa.Cidade = "São paulo";
+        pessoa.Idade = 25;
+
+        pessoa.Apresentar();
+
+
+        Produto produto1 = new Produto();
+        produto1.Nome = "Cerveja Skol lata 273 Ml";
+        produto1.Quantidade = 12;
+        produto1.Preco = 3.99;
+
+
+        produto1.MostrarProduto();
+
+        Carro velho = new Carro();
+        velho.Modelo = "Kaddet";
+        velho.Marca = "Volkswagen";
+        velho.Ano = 1980;
+        velho.Cor = "Vermelho";
+
+        velho.MostrarCarro();
+
+        Aluno aluno = new Aluno();
+        aluno.Nome = "Vinicius";
+        aluno.Curso = "Eng Software";
+        aluno.Nota01 = 5.3;
+        aluno.Nota02 = 9.4;
+        aluno.CalcularMedia();
+
+        ContaBancaria Conta1 = new ContaBancaria();
+
+        Conta1.titular = "João";
+        Conta1.saldo = 0;
+        Conta1.Deposito(900);
+        Conta1.Sacar(100);
+        Conta1.MostrarSaldo();
+
+        Funcionario P6 = new Funcionario();
+        P6.Nome = "Carlos";
+        P6.Cargo = "Analista";
+        P6.Salario = 3000;
+        P6.AumentarSalario(10);
+
+        Telefone telefone = new Telefone();
+        telefone.Marca = "Xiomi";
+        telefone.Modelo = "Redmi Note 8";
+        telefone.Bateria = 85;
+
+        telefone.Ligar();
+        telefone.MistrarBateria();
+        telefone.Desligar();
+    }
+}
