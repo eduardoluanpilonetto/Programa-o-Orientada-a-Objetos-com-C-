adb5dbb61f17b09101e791b84b7e6a2689551a8a
Eduardo Luan Pilonetto <eduardo.pilonetto@logae.com.br>
2026-08-03 21:21:15 -0300

    Fazendo primeiros Exercícios

---
+++ Aula 3/CorrecaoExercicios/Classes.cs
@@ -0,0 +1,141 @@
+﻿namespace Classes
+{
+    internal class Pessoa
+    {
+        public string nome;
+        private int idade;
+
+        public int GetIdade()
+        {
+            return idade;
+        }
+
+        public void SetIdade(int valor)
+        {
+            if (valor > 0) idade = valor;
+            else Console.WriteLine("Idade Inválida");
+        }
+    }
+
+    class Produto
+    {
+        public string nome;
+        private double preco;
+        public double Preco
+        {
+            get { return preco; }
+            set
+            {
+                if (value > 0) preco = value;
+                else Console.WriteLine("Valor Invalido");
+            }
+        }
+
+        public void AplicarDesconto(double valor)
+        {
+            if (valor < preco) preco = preco - valor;
+            else Console.WriteLine("Valor Invalido");
+        }
+    }
+
+    class ContaBancaria
+    {
+        public string titular;
+        private double saldo;
+        public double Saldo
+        {
+            get { return saldo; }
+            private set { saldo = value; }
+        }
+
+        public void Depositar(double valor)
+        {
+            if(valor <= 0)
+            {
+                Console.WriteLine("Valor Inválido");
+            }
+            else
+            {
+                saldo = valor;
+            }
+        }
+
+        public void Sacar(double valor)
+        {
+            if(valor > saldo)
+            {
+                Console.WriteLine("Saque Indisponível");
+            }
+            else
+            {
+                saldo = saldo - valor;
+            }
+        }
+    }
+
+    public class Carro
+    {
+        private int velocidade;
+        public int Velocidade
+        {
+            get { return velocidade; }
+        }
+
+        public void Acelerar(int valor)
+        {
+            if (valor > 0)
+            {
+                velocidade += valor;
+
+                if (velocidade > 220)
+                {
+                    velocidade = 220;
+                }
+            }
+        }
+
+        // Método para frear
+        public void Frear(int valor)
+        {
+            if (valor > 0)
+            {
+                velocidade -= valor;
+
+                if (velocidade < 0)
+                {
+                    velocidade = 0;
+                }
+            }
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome;
+        private double nota1;
+        private double nota2;
+
+        public void DefinirNotas(double n1, double n2)
+        {
+            if (n1 >= 0 && n1 <= 10)
+                nota1 = n1;
+            else
+                Console.WriteLine("A nota 1 deve estar entre 0 e 10.");
+
+            if (n2 >= 0 && n2 <= 10)
+                nota2 = n2;
+            else
+                Console.WriteLine("A nota 2 deve estar entre 0 e 10.");
+        }
+
+        public double CalcularMedia()
+        {
+            return (nota1 + nota2) / 2;
+        }
+
+        public bool VerificarAprovacao()
+        {
+            return CalcularMedia() >= 7.0;
+        }
+    }
+}
---
+++ Aula 3/CorrecaoExercicios/Program.cs
@@ -0,0 +1,43 @@
+﻿using Classes;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Pessoa pes = new Pessoa();
+        pes.nome = "Duduzinho";
+        pes.SetIdade(10);
+        Console.WriteLine(pes.GetIdade());
+
+        Produto produto = new Produto();
+        produto.nome = "Sabonete";
+        produto.Preco = 10;
+        produto.AplicarDesconto(5);
+
+        ContaBancaria contaBancaria = new ContaBancaria();
+        contaBancaria.titular = "Pilonetto";
+        contaBancaria.Depositar(10);
+        Console.WriteLine(contaBancaria.Saldo);
+        contaBancaria.Sacar(5);
+        Console.WriteLine(contaBancaria.Saldo);
+       
+        Carro carro = new Carro();
+        carro.Acelerar(100);
+        Console.WriteLine(carro.Velocidade);
+        carro.Acelerar(150);
+        Console.WriteLine(carro.Velocidade);
+        carro.Frear(50);
+        Console.WriteLine(carro.Velocidade); 
+        carro.Frear(300);
+        Console.WriteLine(carro.Velocidade);
+
+        Aluno aluno = new Aluno();
+        aluno.Nome = "João";
+        aluno.DefinirNotas(8.5, 7.0);
+        Console.WriteLine("Nome: " + aluno.Nome);
+        Console.WriteLine("Média: " + aluno.CalcularMedia());
+        if (aluno.VerificarAprovacao())
+            Console.WriteLine("Situação: Aprovado");
+        else
+            Console.WriteLine("Situação: Reprovado");
+    }
+}
