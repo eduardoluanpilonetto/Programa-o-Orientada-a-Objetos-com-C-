8862910fd87460ac2e26bf3aef8521c7d9e6132a
Eduardo Luan Pilonetto <eduardo.pilonetto@logae.com.br>
2026-08-17 19:30:45 -0300

    Criando Projeto Exemplo

---
+++ Aula 5/ExemploPratico/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Cachorro meuDoguinho = new Cachorro();
+        meuDoguinho.Nome = "Eduardo";
+        meuDoguinho.Comer();
+        meuDoguinho.EmitirSom();
+
+        Gato meuFelino = new Gato();
+        meuFelino.Nome = "Mimi";
+        meuFelino.Comer();
+        meuFelino.EmitirSom();
+    }
+}
+
+public class Animal
+{
+    public string Nome;
+
+    public virtual void EmitirSom()
+    {
+        Console.WriteLine("Som do animal");
+    }
+
+    public void Comer()
+    {
+        Console.WriteLine("Comendo...");
+    }
+}
+
+public class Cachorro : Animal
+{
+    public override void EmitirSom()
+    {
+        Console.WriteLine("Au Au!");
+    }
+}
+
+public class Gato : Animal
+{
+    public override void EmitirSom()
+    {
+        Console.WriteLine("Miau!");
+    }
+}
